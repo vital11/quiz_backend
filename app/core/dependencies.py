3a7153b49e1f1fe435d98640bb7688ef77ec318c
vital11 <1861755@gmail.com>
2025-03-23 07:13:49 +0200

    feat: add expire JWT check

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Annotated
 
 from fastapi import Depends, HTTPException, status
@@ -26,6 +27,11 @@ def verify_token(token: TokenDep) -> TokenPayload:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Could not validate credentials",
+        )
+    if payload.exp < datetime.now(tz=timezone.utc):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Access token expired"
         )
     return payload
 
