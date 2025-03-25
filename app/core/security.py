a1acb9623ac0028615eadd9a60ae808399db4c79
vital11 <1861755@gmail.com>
2025-03-25 22:27:23 +0200

    refactor: replace passlib with bcrypt

--- app/core/security.py
+++ app/core/security.py
@@ -2,28 +2,29 @@ from datetime import datetime, timedelta, timezone
 from typing import Any
 
 import jwt
-from passlib.context import CryptContext
+import bcrypt
 
 from app.auth.models import TokenPayload
 from app.core.config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> bytes:
+    return bcrypt.hashpw(password=password.encode(), salt=bcrypt.gensalt())
 
 
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
-
-
-def verify_password(password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(password, hashed_password)
+def verify_password(password: str, hashed_password: bytes) -> bool:
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password,
+    )
 
 
 def encode_access_token(
-        payload: TokenPayload,
-        private_key: str = settings.jwt.PRIVATE_KEY_PATH.read_text(),
-        algorithm: str = settings.jwt.ALGORITHM,
-        expire_minutes: int = settings.jwt.ACCESS_TOKEN_EXPIRE_MINUTES,
-        expire_delta: timedelta | None = None,
+    payload: TokenPayload,
+    private_key: str = settings.jwt.PRIVATE_KEY_PATH.read_text(),
+    algorithm: str = settings.jwt.ALGORITHM,
+    expire_minutes: int = settings.jwt.ACCESS_TOKEN_EXPIRE_MINUTES,
+    expire_delta: timedelta | None = None,
 ) -> str:
     to_encode = payload.model_dump(exclude_unset=True)
     now = datetime.now(tz=timezone.utc)
@@ -44,9 +45,9 @@ def encode_access_token(
 
 
 def decode_access_token(
-        token: str | bytes,
-        public_key: str = settings.jwt.PUBLIC_KEY_PATH.read_text(),
-        algorithm: str = settings.jwt.ALGORITHM,
+    token: str | bytes,
+    public_key: str = settings.jwt.PUBLIC_KEY_PATH.read_text(),
+    algorithm: str = settings.jwt.ALGORITHM,
 ) -> Any:
     return jwt.decode(
         token,
